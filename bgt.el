e85eb76809bd0082fc12403e0096b34364b9a422
md-arif-shaikh <arifshaikh.astro@gmail.com>
2021-12-31 16:20:16 +0530

    major revision

--- bgt.el
+++ bgt.el
@@ -1,9 +1,12 @@
-;;; bgt.el --- record blood glucose level with emacs  -*- lexical-binding: t; -*-
+;;; bgt.el --- Record and view blood glucose level  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2021  Md Arif Shaikh
 
 ;; Author: Md Arif Shaikh <arifshaikh.astro@gmail.com>
-;; Keywords:
+;; Homepage: https://github.com/md-arif-shaikh/bgt
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: convenience, glucose monitoring
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -18,8 +21,9 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-;;; Commentary: This is simple package to help you keep your glucose level
-;; with emacs and export data to csv or even plot the data to see the trend.
+;;; Commentary:
+;; This is simple package to help you keep your glucose level
+;; with Emacs and export data to csv or even plot the data to see the trend.
 ;; Plotting would require python with pandas and matplotlib installed on your
 ;; machine.
 
@@ -27,74 +31,97 @@
 
 (require 'org)
 
-(defcustom bgt-file-name "/tmp/bgt.org"
+(defcustom bgt-file-name nil
   "Org file name to save the blood glucose data."
   :type 'string
-  :group 'bgt
-  )
+  :group 'bgt)
 
-(defcustom bgt-csv-file-name "/tmp/bgt.csv"
+(defcustom bgt-csv-file-name nil
   "CSV file name for exporting the table data from org file."
   :type 'string
   :group 'bgt)
 
-(defcustom bgt-python-file "~/.emacs.d/bgt.py"
+(defcustom bgt-python-file nil
   "Path to the python script for plotting."
   :type 'string
   :group 'bgt)
 
+(defcustom bgt-python-path nil
+  "Path to python."
+  :type 'string
+  :group 'bgt)
+
+(defcustom bgt-plot-file nil
+  "File name to save the plot."
+  :type 'string
+  :group 'bgt)
+
+
+(defun bgt-create-initial-file ()
+  "Create the data file with initial inputs when the file does not exist."
+  (unless bgt-file-name
+    (error "`bgt-file-name` can not be nil.  Set it using `(setq bgt-file-name '/path-to-file/filename.org')`!"))
+  (unless (string-equal "org" (file-name-extension bgt-file-name))
+    (error "`bgt-file-name` should be an `org` file!"))
+  (unless bgt-csv-file-name
+    (error "`bgt-csv-file-name` can not be nil.  Set it using `(setq bgt-csv-file-name '/path-to-file/filename.csv')`!"))
+  (with-current-buffer (generate-new-buffer bgt-file-name)
+    (insert "#+TITLE: Blood Glucose Table\n\n")
+    (insert "* BGT\n")
+    (insert ":PROPERTIES:\n")
+    (insert (format ":TABLE_EXPORT_FILE: %s\n" bgt-csv-file-name))
+    (insert ":TABLE_EXPORT_FORMAT: orgtbl-to-csv\n")
+    (insert ":END:\n\n")
+    (insert "|--|--|--|--|--|\n")
+    (insert "|Date |BG | Category | Test | Lab|\n")
+    (insert "|--|--|--|--|--|\n")
+    (write-file bgt-file-name)))
+
 (defun bgt-add-entry ()
   "Add bg record."
   (interactive)
-  (let ((file-name bgt-file-name)
-	(date-time (org-read-date 'with-time nil nil "Record time:  "))
+  (let ((date-time (org-read-date 'with-time nil nil "Record time:  "))
 	(bg-level (read-string "BG level: "))
 	(bg-category (completing-read "Record type: " '("Fasting" "Random" "Post-prandial")))
 	(bg-test (completing-read "Test type: " '("Plasma" "Capillary")))
 	(bg-lab (completing-read "Lab Name: " '("Glucometer"))))
-    (with-temp-buffer
-      (when (not (file-exists-p file-name))
-	;;(set-buffer (generate-new-buffer file-name))
-	(insert "#+TITLE: Blood Glucose Table\n\n")
-	(insert "* BGT\n")
-	(insert ":PROPERTIES:\n")
-	(insert (format ":TABLE_EXPORT_FILE: %s\n" bgt-csv-file-name))
-	(insert ":TABLE_EXPORT_FORMAT: orgtbl-to-csv\n")
-	(insert ":END:\n\n")
-	(insert "|--|--|--|--|--|\n")
-	(insert "|Date |BG | Category | Test | Lab|\n")
-	(insert "|--|--|--|--|--|\n")
-	(append-to-file (point-min) (point-max) file-name)))
+    (unless (file-exists-p bgt-file-name)
+      (bgt-create-initial-file))
     (with-temp-buffer
       (insert (format "|%s |%s |%s |%s |%s |\n" date-time bg-level bg-category bg-test bg-lab))
-      (insert "|--|--|--|--|--|\n")
-      (append-to-file (point-min) (point-max) file-name))
-    (with-temp-buffer
-      (switch-to-buffer (find-file-noselect file-name))
+      (append-to-file (point-min) (point-max) bgt-file-name)
+      (switch-to-buffer (find-file-noselect bgt-file-name))
       (goto-char (point-max))
       (forward-line -1)
       (org-table-align)
-      (write-file file-name))))
+      (write-file bgt-file-name))))
 
 (defun bgt-export-to-csv ()
   "Export bgt data to a csv file."
   (interactive)
-  (let ((file-name bgt-file-name))
-    (with-temp-buffer
-      (switch-to-buffer (find-file-noselect file-name))
-      (goto-char (point-max))
-      (forward-line -2)
-      (org-table-export))))
+  (with-current-buffer (find-file-noselect bgt-file-name)
+    (goto-char (point-max))
+    (forward-line -2)
+    (org-table-export)))
 
-(defun bgt-plot ()
-  "Plot BG data."
-  (interactive)
-  (let ((bgt-data-file-name bgt-csv-file-name))
-    (bgt-export-to-csv)
-    (cond ((file-exists-p bgt-python-file)
-	   (shell-command (format "python %s --data_file %s" bgt-python-file bgt-data-file-name))
-	   (find-file-other-frame "/tmp/bgt.pdf"))
-	  (t (message "python file not found. Set the python file path with 'setq bgt-python-file'.")))))
+(defun bgt-plot (start-date end-date)
+  "Plot BG data from START-DATE to END-DATE."
+  (interactive
+   (list (org-read-date nil nil nil "Start Date: ")
+	 (org-read-date nil nil nil "End Date: ")))
+  (bgt-export-to-csv)
+  (unless bgt-python-file
+    (error "`bgt-python-file` can not be nil.  'setq bgt-python-file'!"))
+  (unless bgt-plot-file
+    (setq bgt-plot-file (concat (temporary-file-directory) "bgt.pdf")))
+  (when (file-exists-p bgt-plot-file)
+    (delete-file bgt-plot-file))
+  (if (file-exists-p bgt-python-file)
+      (shell-command (format "%s %s --data_file %s --plot_file %s --start_date %s --end_date %s"
+			     (or bgt-python-path "python") bgt-python-file bgt-csv-file-name bgt-plot-file start-date end-date))
+    (message "python file not found. Set the python file path with 'setq bgt-python-file'."))
+  (when (file-exists-p bgt-plot-file)
+    (find-file-other-window bgt-plot-file)))
 
 (provide 'bgt)
 ;;; bgt.el ends here
--- bgt.py
+++ bgt.py
@@ -1,8 +1,27 @@
 # Plot blood glucose level
+# This part of the Emacs package `bgt`
+# Copyright (C) 2021  Md Arif Shaikh
+# Author: Md Arif Shaikh
+# Email: arifshaikh.astro@gmail.com
+# Version: 0.1.0
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 
@@ -12,11 +31,55 @@ parser.add_argument(
     required=True,
     help="Data file containing bgt data."
 )
+parser.add_argument(
+    "--plot_file",
+    type=str,
+    required=True,
+    help="file name for plot."
+)
+parser.add_argument(
+    "--start_date",
+    type=str,
+    required=True,
+    help="Start date for range of plot."
+)
+parser.add_argument(
+    "--end_date",
+    type=str,
+    required=True,
+    help="End date for range of plot."
+)
 
 args = parser.parse_args()
 
-file_name = args.data_file
-df = pd.read_csv(file_name)
-fig, ax = plt.subplots()
-df.plot("Date", "BG", ax=ax, marker=".", grid=True, xlabel="Dates", ylabel="Blood Glucose Level")
-fig.savefig("/tmp/bgt.pdf")
+data_file = args.data_file
+plot_file = args.plot_file
+start_date = args.start_date
+end_date = args.end_date
+
+df = pd.read_csv(data_file)
+start_date = datetime.strptime(start_date, "%Y-%m-%d")
+end_date = datetime.strptime(end_date, "%Y-%m-%d")
+
+df["date"] = [datetime.strptime(date[:10], "%Y-%m-%d") for date in df.Date]
+df_fasting = df.loc[(df.Category == "Fasting")
+                    & ((df.date >= start_date) & (df.date <= end_date))]
+df_random = df.loc[(df.Category == "Random")
+                   & ((df.date >= start_date) & (df.date <= end_date))]
+df_pp = df.loc[(df.Category == "Post-prandial")
+               & ((df.date >= start_date) & (df.date <= end_date))]
+
+fig, ax = plt.subplots(figsize=(6, 4))
+ax.plot(df_fasting["date"], df_fasting["BG"], marker=".", label="Fasting")
+ax.plot(df_random["date"], df_random["BG"], marker=".", label="Random")
+ax.plot(df_pp["date"], df_pp["BG"], marker=".", label="PP")
+ax.legend()
+ax.grid(ls="--")
+ax.axhline(130, c="tab:blue", alpha=0.5)
+ax.axhline(180, c="tab:green", alpha=0.5)
+ax.set_ylabel("Glucose level")
+ax.set_xlabel("Dates")
+plt.xticks(rotation=45)
+plt.subplots_adjust(bottom=0.25, top=0.95, right=0.95)
+
+fig.savefig(plot_file)
